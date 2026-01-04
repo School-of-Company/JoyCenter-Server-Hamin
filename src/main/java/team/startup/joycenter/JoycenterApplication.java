9799094f06bf3c64c2b52920d2b5cedad7a293fe
박하민 <hamin090403@gmail.com>
2026-01-04 15:07:38 +0900

    init

---
+++ src/main/java/team/startup/joycenter/JoycenterApplication.java
@@ -0,0 +1,13 @@
+package team.startup.joycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoycenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoycenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/startup/joycenter/JoycenterApplicationTests.java
@@ -0,0 +1,13 @@
+package team.startup.joycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoycenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
