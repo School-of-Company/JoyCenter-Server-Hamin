43fd912fa4611d2e1b4b764a6ee4fc78864c2157
박하민 <hamin090403@gmail.com>
2026-01-06 23:16:53 +0900

    add :: ErrorResponse 추가

---
+++ src/main/java/team/startup/joycenter/global/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package team.startup.joycenter.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private int status;
+    private String message;
+}
