6c421a07f7f7d31ee689d3d8e7e328bc783be8cb
박하민 <hamin090403@gmail.com>
2026-01-09 23:58:36 +0900

    add :: RefreshToken 추가

---
+++ src/main/java/team/startup/joycenter/domain/auth/entity/RefreshToken.java
@@ -0,0 +1,20 @@
+package team.startup.joycenter.domain.auth.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+import team.startup.joycenter.global.security.jwt.JwtProvider;
+
+@RedisHash(value = "gami_refreshToken", timeToLive = JwtProvider.REFRESH_TOKEN_TIME)
+@Getter
+@Builder
+public class RefreshToken {
+
+    @Id
+    private Long memberId;
+
+    @Indexed
+    private String token;
+}
