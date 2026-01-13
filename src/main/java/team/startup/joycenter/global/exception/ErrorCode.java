94f7ac1ab7633bed844988c905ffaed74db954c1
박하민 <hamin090403@gmail.com>
2026-01-13 19:44:07 +0900

    add :: ErrorCode 추가

--- src/main/java/team/startup/joycenter/global/exception/ErrorCode.java
+++ src/main/java/team/startup/joycenter/global/exception/ErrorCode.java
@@ -14,6 +14,10 @@ public enum ErrorCode {
 
     NOT_FOUND_MEMBER(404, "존재하지 않는 사용자입니다."),
 
+    NOT_FOUND_ATTACHMENTS(404, "존재하지 않는 첨부파일입니다."),
+    ATTACHMENTS_UPLOAD_FAILED(500, "첨부파일 업로드에 실패했습니다."),
+    ATTACHMENTS_DELETE_FAILED(500, "첨부파일 삭제에 실패했습니다."),
+
     NOT_FOUND_POST(404, "존재하지 않는 게시글입니다.");
 
     private final int status;
