26178b902144ac6fbf53f1ac641c9d8d79c2662d
박하민 <hamin090403@gmail.com>
2026-01-13 19:43:51 +0900

    add :: 첨부파일 매핑 로직 추가

--- src/main/java/team/startup/joycenter/domain/post/service/impl/CreatePostServiceImpl.java
+++ src/main/java/team/startup/joycenter/domain/post/service/impl/CreatePostServiceImpl.java
@@ -3,6 +3,8 @@ package team.startup.joycenter.domain.post.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.startup.joycenter.domain.attachments.entity.Attachments;
+import team.startup.joycenter.domain.attachments.repository.AttachmentsRepository;
 import team.startup.joycenter.domain.member.entity.Member;
 import team.startup.joycenter.domain.post.dto.request.CreatePostRequest;
 import team.startup.joycenter.domain.post.entity.Post;
@@ -10,17 +12,21 @@ import team.startup.joycenter.domain.post.repostory.PostRepository;
 import team.startup.joycenter.domain.post.service.CreatePostService;
 import team.startup.joycenter.global.util.MemberUtil;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CreatePostServiceImpl implements CreatePostService {
 
     private final PostRepository postRepository;
     private final MemberUtil memberUtil;
+    private final AttachmentsRepository attachmentsRepository;
 
     @Override
     @Transactional
     public void execute(CreatePostRequest request) {
         Member member = memberUtil.getCurrentMember();
+        List<Attachments> attachments = attachmentsRepository.findByIdIn(request.attachmentsIds());
 
         Post post = Post.builder()
                 .author(member)
@@ -30,5 +36,9 @@ public class CreatePostServiceImpl implements CreatePostService {
                 .build();
 
         postRepository.save(post);
+
+        for (Attachments attachment : attachments) {
+            attachment.setPost(post);
+        }
     }
 }
