f390ff91d641aa5fccd11c72600ff89678febb1f
박하민 <hamin090403@gmail.com>
2026-01-17 21:32:41 +0900

    add :: findS3KeysByPostId 메소드 추가

--- src/main/java/team/startup/joycenter/domain/attachments/repository/AttachmentsRepository.java
+++ src/main/java/team/startup/joycenter/domain/attachments/repository/AttachmentsRepository.java
@@ -1,13 +1,16 @@
 package team.startup.joycenter.domain.attachments.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import team.startup.joycenter.domain.attachments.entity.Attachments;
 import team.startup.joycenter.domain.attachments.entity.constant.AttachmentsType;
 
 import java.util.List;
 
 public interface AttachmentsRepository extends JpaRepository<Attachments, Long> {
-    List<Attachments> findByIdIn(List<Long> AttachmentsId);
-    List<Attachments> findAllByPostIdOrderByImageOrderAsc(Long postId);
+    List<Attachments> findByPostId(Long postId);
     List<Attachments> findAllByPostIdInAndAttachmentsTypeOrderByPostIdAscImageOrderAsc(List<Long> postIds, AttachmentsType type);
+    @Query("select a.s3Key from Attachments a where a.post.id = :postId")
+    List<String> findS3KeysByPostId(@Param("postId") Long postId);
 }
