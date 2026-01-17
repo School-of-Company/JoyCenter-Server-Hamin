8759c1277b92c153ee181268845928897dc1ccdb
박하민 <hamin090403@gmail.com>
2026-01-17 21:31:34 +0900

    add :: UpdatePostRequest 추가

---
+++ src/main/java/team/startup/joycenter/domain/post/dto/request/UpdatePostRequest.java
@@ -0,0 +1,19 @@
+package team.startup.joycenter.domain.post.dto.request;
+
+import software.amazon.awssdk.annotations.NotNull;
+import team.startup.joycenter.domain.post.entity.constant.BlockType;
+
+import java.util.List;
+
+public record UpdatePostRequest(
+        @NotNull String title,
+        @NotNull List<BlockRequest> blocks
+) {
+    public record BlockRequest(
+            @NotNull Integer order,
+            @NotNull BlockType blockType,
+            String text,
+            Long attachmentId
+    ) {}
+}
+
