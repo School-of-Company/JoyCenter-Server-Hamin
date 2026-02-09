9f9d25c30f619b6eef02516f7a3ceb57f0f1a769
박하민 <hamin090403@gmail.com>
2026-02-09 16:01:40 +0900

    add :: 필요 메소드 추가

--- src/main/java/team/startup/joycenter/domain/attachments/repository/AttachmentsRepository.java
+++ src/main/java/team/startup/joycenter/domain/attachments/repository/AttachmentsRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import team.startup.joycenter.domain.attachments.entity.Attachments;
 import team.startup.joycenter.domain.attachments.entity.constant.AttachmentsType;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AttachmentsRepository extends JpaRepository<Attachments, Long> {
@@ -13,4 +14,7 @@ public interface AttachmentsRepository extends JpaRepository<Attachments, Long> 
     List<Attachments> findAllByPostIdInAndAttachmentsTypeOrderByPostIdAscImageOrderAsc(List<Long> postIds, AttachmentsType type);
     @Query("select a.s3Key from Attachments a where a.post.id = :postId")
     List<String> findS3KeysByPostId(@Param("postId") Long postId);
+
+    List<Attachments> findByPostIsNullAndCreatedAtBefore(LocalDateTime time);
+    void deleteByS3KeyIn(List<String> s3Keys);
 }
